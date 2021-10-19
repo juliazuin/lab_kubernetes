72124d6543ebf02bc8bb75775064490a98c28266
Julia Zuin <zuinjulia@hotmail.com>
2021-10-19 10:32:32 -0300

    terraform ajustes

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "acessos_master" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids = null,
       security_groups: null,
