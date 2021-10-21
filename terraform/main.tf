fdbc4343036926e14b6d967572cc829a7087c798
Julia Zuin <zuinjulia@hotmail.com>
2021-10-21 10:43:45 -0300

    criando hproxy

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,6 +59,7 @@ resource "aws_instance" "k8s_workers" {
 resource "aws_security_group" "acessos_master" {
   name        = "k8s-acessos_master"
   description = "acessos inbound traffic"
+  vpc_id = "vpc-063fc945cde94d3ab"
 
   ingress = [
     {
@@ -109,6 +110,7 @@ resource "aws_security_group" "acessos_master" {
 resource "aws_security_group" "acessos" {
   name        = "k8s-workers"
   description = "acessos inbound traffic"
+  vpc_id = "vpc-063fc945cde94d3ab"
 
   ingress = [
     {
